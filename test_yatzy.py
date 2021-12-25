98c35b53ff71bd945ee921d77eaf147cd6274611
nicolaujoan <jnicolausantandreu@cifpbmoll.eu>
2021-12-25 13:34:42 +0100

    :white_check_mark: refactor y casos tests pasados

--- test_yatzy.py
+++ test_yatzy.py
@@ -11,17 +11,69 @@ def test_chance():
     assert 14 == Yatzy.chance(1, 1, 3, 3, 6)
     assert 21 == Yatzy.chance(4, 5, 5, 6, 1)
 
+def test_yatzy():
+    assert Yatzy.yatzy(1, 1, 1, 1, 1) == 50
+    assert Yatzy.yatzy(1, 2, 3, 4, 5) == 0
+
 
 @pytest.fixture
 def inyector():
     # Es el setup de unittest o de JUnit
-    tirada = Yatzy(1, 2, 3, 4, 5)
+    tirada = Yatzy(5, 6, 4, 4, 5)
     return tirada
 
 
 def test_fours(inyector):
     # Es necesario un objeto ya creado
-    valorEsperado = 4
+    valorEsperado = 8
     # No puedo testear con fixtures = inyeccion de dependencias
     # los metodos estaticos como chance()
     assert valorEsperado == inyector.fours()
+
+def test_fives(inyector):
+    valorEsperado = 10
+    assert valorEsperado == inyector.fives()
+
+def test_sixes(inyector):
+    valorEsperado = 6
+    assert valorEsperado == inyector.sixes()
+
+def test_score_pair():
+    assert Yatzy.score_pair(3, 3, 3, 4, 4) == 8
+    assert Yatzy.score_pair(1, 1, 6, 2, 6) == 12
+    assert Yatzy.score_pair(3, 3, 3, 4, 1) == 6
+    assert Yatzy.score_pair(3, 3, 3, 3, 1) == 6
+    assert Yatzy.score_pair(1, 2, 3, 4, 5) == 0
+
+def test_two_pair():
+    assert Yatzy.two_pair(1, 1, 2, 3, 3) == 8
+    assert Yatzy.two_pair(1, 1, 2, 3, 4) == 0
+    assert Yatzy.two_pair(1, 1, 2, 2, 2) == 6
+
+def test_four_of_a_kind():
+    assert Yatzy.four_of_a_kind(2, 2, 2, 2, 5) == 8
+    assert Yatzy.four_of_a_kind(2, 2, 2, 5, 5) == 0
+    assert Yatzy.four_of_a_kind(2, 2, 2, 2, 2) == 8
+
+def test_three_of_a_kind():
+    assert Yatzy.three_of_a_kind(3, 3, 3, 4, 5) == 9
+    assert Yatzy.three_of_a_kind(3, 3, 4, 5, 6) == 0
+    assert Yatzy.three_of_a_kind(3, 3, 3, 3, 1) == 9
+
+def test_smallStraight():
+    assert Yatzy.smallStraight(1, 2, 3, 4, 5) == 15
+    assert Yatzy.smallStraight(1, 2, 2 ,3, 4, 5) == 0
+    assert Yatzy.smallStraight(2, 4, 3, 3, 1) == 0
+
+def test_largeStraight():
+    assert Yatzy.largeStraight(2, 3, 4, 5, 6) == 20
+    assert Yatzy.largeStraight(1, 2, 3, 4, 5) == 0
+    assert Yatzy.largeStraight(1, 3, 3, 2, 6) == 0
+
+def test_fullHouse():
+    assert Yatzy.fullHouse(1, 1, 2, 2, 2) == 8
+    assert Yatzy.fullHouse(2, 3, 3, 3, 2) == 13
+    assert Yatzy.fullHouse(2, 2, 3, 3, 4) == 0
+    assert Yatzy.fullHouse(4, 4, 4 ,4, 4) == 0
+
+
--- yatzy.py
+++ yatzy.py
@@ -1,230 +1,130 @@
 class Yatzy:
 
+    def __init__(self, *dice):
+        self.dice = list(dice)
+        
+        
     @staticmethod
-    def chance(d1, d2, d3, d4, d5):
-        total = 0
-        total += d1
-        total += d2
-        total += d3
-        total += d4
-        total += d5
-        return total
+    def chance(*dice):
+        return sum(dice)    
+
 
     @staticmethod
-    def yatzy(dice):
-        counts = [0]*(len(dice)+1)
-        for die in dice:
-            counts[die-1] += 1
-        for i in range(len(counts)):
-            if counts[i] == 5:
-                return 50
+    def yatzy(*dice):
+        if dice[:-1] == dice[1:]:
+            return 50
         return 0
     
-    @staticmethod
-    def ones( d1,  d2,  d3,  d4,  d5):
-        sum = 0
-        if (d1 == 1):
-            sum += 1
-        if (d2 == 1):
-            sum += 1
-        if (d3 == 1):
-            sum += 1
-        if (d4 == 1):
-            sum += 1
-        if (d5 == 1): 
-            sum += 1
-
-        return sum
-    
 
     @staticmethod
-    def twos( d1,  d2,  d3,  d4,  d5):
-        sum = 0
-        if (d1 == 2):
-             sum += 2
-        if (d2 == 2):
-             sum += 2
-        if (d3 == 2):
-             sum += 2
-        if (d4 == 2):
-             sum += 2
-        if (d5 == 2):
-             sum += 2
-        return sum
+    def ones(*dice):
+        ONE = 1
+        return dice.count(ONE) * ONE
+
     
     @staticmethod
-    def threes( d1,  d2,  d3,  d4,  d5):
-        s = 0
-        if (d1 == 3):
-             s += 3
-        if (d2 == 3):
-             s += 3
-        if (d3 == 3):
-             s += 3
-        if (d4 == 3):
-             s += 3
-        if (d5 == 3):
-             s += 3
-        return s
-    
+    def twos(*dice):
+        TWO = 2
+        return dice.count(TWO) * TWO
 
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
+    
+    @staticmethod
+    def threes(*dice):
+            THREE = 3
+            return dice.count(THREE) * THREE
+        
     
     def fours(self):
-        sum = 0
-        for at in range(5):
-            if (self.dice[at] == 4): 
-                sum += 4
-        return sum
+        FOUR = 4
+        return self.dice.count(FOUR) * FOUR
     
 
     def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)): 
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
+      FIVE = 5
+      return self.dice.count(FIVE) * FIVE
     
 
     def sixes(self):
-        sum = 0
-        for at in range(len(self.dice)): 
-            if (self.dice[at] == 6):
-                sum = sum + 6
-        return sum
+        SIX = 6
+        return self.dice.count(SIX) * SIX
     
     @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
+    def score_pair(*dice):
+        mydice = list(dice)
+        matching_dice_found = False
+        PAIR = 2
+        while not matching_dice_found and len(mydice) >= PAIR:
+            biggest = max(mydice)
+            if mydice.count(biggest) >= PAIR:
+                matching_dice_found = True
+                return biggest * PAIR
+            else:
+                mydice = list(filter((biggest).__ne__, mydice))
         return 0
-    
+
     @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
+    def two_pair(*dice):
+        mydice = list(dice)
+        double_matching_dice_found = False
+        PAIR = 2
+        matchings = 0
         score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
-    
-    @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
+        while not double_matching_dice_found and len(mydice) >= PAIR:
+            biggest = max(mydice)
+            if mydice.count(biggest) >= PAIR:
+                matchings += 1
+                score += biggest * PAIR
+                if matchings == PAIR:
+                    double_matching_dice_found = True
+                    return score
+            mydice = list(filter((biggest).__ne__, mydice))
         return 0
-    
-
-    @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
-    
-
-    @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
-            return 15
-        return 0
-    
-
-    @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
-            return 20
-        return 0
-    
-
-    @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
             
+    @staticmethod
+    def four_of_a_kind(*dice):
+        FOUR = 4
+        for die in range(max(dice), 1, -1):
+            if dice.count(die) >= FOUR:
+                return die * FOUR
+        return 0
+    
 
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
+    @staticmethod
+    def three_of_a_kind(*dice):
+        THREE = 3
+        for die in range(max(dice), 1, -1):
+            if dice.count(die) >= THREE:
+                return die * THREE
+        return 0
+    
 
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
-        else:
-            return 0
+    @staticmethod
+    def smallStraight(*dice):
+        max = 5
+        min = 1
+        small_straight = list(range(min, max + 1))
+        if list(dice) == small_straight: return sum(small_straight)
+        else: return 0
+    
+
+    @staticmethod
+    def largeStraight(*dice):
+        max = 6 
+        min = 2
+        small_straight = list(range(min, max + 1))
+        if list(dice) == small_straight: return sum(small_straight)
+        else: return 0
+    
+    @staticmethod
+    def __two_of_a_kind(*dice):
+        TWO = 2
+        for die in range(max(dice), 0, -1):
+            if dice.count(die) == TWO:
+                return die * TWO
+        return 0
+
+    @staticmethod
+    def fullHouse(*dice):
+        if Yatzy.__two_of_a_kind(*dice) and Yatzy.three_of_a_kind(*dice):
+            return Yatzy.__two_of_a_kind(*dice) + Yatzy.three_of_a_kind(*dice)  
+        return 0
